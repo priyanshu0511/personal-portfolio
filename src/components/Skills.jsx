3b305ea307b69f917d84888e2db59b1f42d544e1
priyanshu05112 <priyanshu05112@gmail.com>
2025-11-26 12:35:08 +0530

    added technologies

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -5,85 +5,95 @@ import { motion } from "motion/react";
 const Skills = () => {
   const technologies = [
   {
-    id: 1,
+    id:1,
+    src: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/nextjs/nextjs-original.svg",
+    name: "Next.JS",
+  },
+
+    {
+    id: 2,
     src: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/react/react-original.svg",
     name: "React.JS",
   },
   {
-    id: 2,
+    id: 3,
     src: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/nodejs/nodejs-original.svg",
     name: "Node.js",
   },
   {
-    id: 3,
+    id: 4,
     src: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/express/express-original.svg",
     name: "Express.js",
-  },
-  {
-    id: 4,
-    src: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/mongodb/mongodb-original.svg",
-    name: "MongoDB",
-  },
-  {
+  },{
     id: 5,
-    src: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/tailwindcss/tailwindcss-original.svg",
-    name: "TailwindCSS",
-  },
-  {
-    id: 6,
     src: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/typescript/typescript-original.svg",
     name: "TypeScript",
   },
   {
+    id: 6,
+    src: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/mongodb/mongodb-original.svg",
+    name: "MongoDB",
+  },
+  {
     id: 7,
+    src: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/tailwindcss/tailwindcss-original.svg",
+    name: "TailwindCSS",
+  },
+  {
+    id: 8,
     src: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/javascript/javascript-original.svg",
     name: "JavaScript",
   },
   {
-    id: 8,
+    id: 9,
     src: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/python/python-original.svg",
     name: "Python",
   },
   {
-    id: 9,
+    id: 10,
     src: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/java/java-original.svg",
     name: "Java",
   },
   {
-    id: 10,
+    id: 11,
     src: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/c/c-original.svg",
     name: "C",
   },
   {
-    id: 11,
+    id: 12,
     src: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/html5/html5-original.svg",
     name: "HTML",
   },
   {
-    id: 12,
+    id: 13,
     src: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/css3/css3-original.svg",
     name: "CSS",
   },
   {
-    id: 13,
+    id: 14,
     src: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/bootstrap/bootstrap-original.svg",
     name: "Bootstrap",
   },
   {
-    id: 14,
+    id: 15,
     src: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/mysql/mysql-original.svg",
     name: "MySQL",
   },
   {
-    id: 15,
+    id: 16,
+    src: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/postgresql/postgresql-original.svg",
+    name: "PostgreSQL",
+  },
+  {
+    id: 17,
     src: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/firebase/firebase-original.svg",
     name: "Firebase",
   },
   {
-    id: 16,
+    id: 18,
     src: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/github/github-original.svg",
     name: "GitHub",
-  },
+  }
 ];
 
 
